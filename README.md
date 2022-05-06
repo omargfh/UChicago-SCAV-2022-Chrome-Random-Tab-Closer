dd3922c6845338c16f0f070cef35c7301c623c6e
Omar Ibrahim <omaribrahim@cs.uchicago.edu>
2022-05-05 21:54:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UChicago SCAV 2022: Chrome Random Tab Closer
+Closes a random Chrome tab in a given window every 0-5 minutes.
---
+++ popup.js
@@ -0,0 +1,19 @@
+const fn = () => {
+    chrome.tabs.query({
+        currentWindow: true
+    }, function (tabs) {
+        let len = tabs.length;
+        if (tabs.length !== 0) {
+            let random_tab = tabs[Math.floor(Math.random()*len)];
+            chrome.tabs.remove(random_tab.id);
+            console.log("Tab closed.");
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById("closeRandomTabBtn");
+    link.addEventListener('click', () => {
+        fn();
+    });
+});
